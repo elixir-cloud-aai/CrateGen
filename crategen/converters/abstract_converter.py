775e72fdfde50e5bb0a8be798418331b3ee57230
salihuDickson <salihudickson@gmail.com>
2024-08-22 13:47:07 +0100

    update lint config and fix lint issue

--- crategen/converters/abstract_converter.py
+++ crategen/converters/abstract_converter.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
 
+
 class AbstractConverter(ABC):
     @abstractmethod
     def convert_to_wrroc(self, data):
         """Convert data to WRROC format"""
-    
+
     @abstractmethod
     def convert_from_wrroc(self, wrroc_data):
         """Convert WRROC data to the original format"""
