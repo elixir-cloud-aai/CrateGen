3159337ee85298d8091ac54d21a5df8e49ef41d3
Karanjot Singh <99573351+Karanjot786@users.noreply.github.com>
2024-10-09 20:44:58 +0530

    style: address linting and typing issues (#29)

--- crategen/converters/abstract_converter.py
+++ crategen/converters/abstract_converter.py
@@ -1,10 +1,29 @@
+"""Abstract base class for converters between TES/WES and WRROC formats."""
+
 from abc import ABC, abstractmethod
 
+
 class AbstractConverter(ABC):
+    """Abstract converter for TES/WES to WRROC and vice versa."""
+
     @abstractmethod
     def convert_to_wrroc(self, data):
-        """Convert data to WRROC format"""
-    
+        """Convert data to WRROC format.
+
+        Args:
+            data: The data to be converted.
+
+        Returns:
+            The data converted to WRROC format.
+        """
+
     @abstractmethod
     def convert_from_wrroc(self, wrroc_data):
-        """Convert WRROC data to the original format"""
+        """Convert WRROC data to the original format.
+
+        Args:
+            wrroc_data: The WRROC data to be converted.
+
+        Returns:
+            The data converted back to the original format.
+        """
